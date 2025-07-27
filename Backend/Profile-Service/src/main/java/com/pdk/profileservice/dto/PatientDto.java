52a9b080c960ff468a77a6f2292ddc2857ad9c1d
Pranav Khaire <pranavkhaire28@gmail.com>
2025-07-27 12:15:31 +0530

    Initialize Profile Service with Spring Boot

---
+++ Backend/Profile-Service/src/main/java/com/pdk/profileservice/dto/PatientDto.java
@@ -0,0 +1,36 @@
+package com.pdk.profileservice.dto;
+
+import java.time.LocalDate;
+
+import com.pdk.profileservice.entity.Patient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PatientDto {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dateOfBirth;
+    private String phone;
+    private String address;
+    private String aadharNumber;
+    private BloodGroup bloodGroup;
+
+    public Patient toEntity() {
+        Patient patient = new Patient();
+        patient.setId(this.id);
+        patient.setName(this.name);
+        patient.setEmail(this.email);
+        patient.setDateOfBirth(this.dateOfBirth);
+        patient.setPhone(this.phone);
+        patient.setAddress(this.address);
+        patient.setAadharNumber(this.aadharNumber);
+        patient.setBloodGroup(this.bloodGroup);
+        return patient;
+    }
+}
---
+++ Backend/Profile-Service/src/main/java/com/pdk/profileservice/service/PatientService.java
@@ -0,0 +1,11 @@
+package com.pdk.profileservice.service;
+
+import com.pdk.profileservice.dto.PatientDto;
+import com.pdk.profileservice.exception.UserException;
+
+public interface PatientService {
+    public Long addPatient(PatientDto patientDto) throws UserException;
+
+    public PatientDto getPatientById(Long id) throws UserException;
+
+}
